aac7e691cc303aab29fe51e043cf64984fcb172f
QinXuYang <1446445040@qq.com>
2020-05-24 15:39:37 +0800

    调整登录接口

--- src/routes/user/login.ts
+++ src/routes/user/login.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express'
+import { Request, Router } from 'express'
 import { find } from '../../db/dao'
 import { USER } from '../../db/model'
 import { sign } from '../../utils/token'
@@ -10,32 +10,45 @@ interface RequestWithBody extends Request {
 }
 
 const router = Router()
-export default router.post('/login', (req: RequestWithBody, res: Response) => {
+
+router.post('/login', (req: RequestWithBody, res) => {
   const { account, password, identity } = req.body
   if (!account || !password || !identity) {
     return res.status(400).end()
   }
-  Promise.all([
-    find(USER, { account, password, identity }), // 账号表
-    find(identity, { account }) // 信息表
-  ]).then(([results, infos]) => {
-    if (results.length === 0) {
-      res.status(401).end()
-    } else {
-      const user = (infos[0] as any)._doc
-      Reflect.deleteProperty(user, 'password')
-      res.status(200)
-      res.cookie('user', JSON.stringify({ account, identity }), {
-        signed: true,
-        httpOnly: true,
-        maxAge: 7 * 24 * 3600 * 1000 // 一周
-      }).json({
-        user,
-        identity,
-        token: sign({ account, identity })
+  find(USER, { 
+    account,
+    password,
+    identity
+  }).then(users => {
+    if (users.length === 0) {
+      res.status(200).json({
+        code: 1,
+        msg: '用户不存在或密码错误'
       })
+      return
+    } else {
+      return find(identity, { account }) // 信息表
     }
+  }).then(result => {
+    if (!result) return
+    const cookieContent = JSON.stringify({ account, identity })
+    res.status(200)
+    res.cookie('user', cookieContent, {
+      signed: true,
+      httpOnly: true,
+      maxAge: 7 * 24 * 3600 * 1000 // 一周
+    }).json({
+      code: 0,
+      msg: '登陆成功',
+      data: {
+        user: { account, identity },
+        token: sign({ account, identity })
+      }
+    })
   }).catch(_ => {
     res.status(500).end()
   })
 })
+
+export default router
